b80bf0fbcf69af42aeae7d3bfc648639a96646f7
NewSarcasmProvider <79085457+NewSarcasmProvider@users.noreply.github.com>
2021-12-15 17:05:41 +0300

    Add files via upload

---
+++ precache-manifest.bbf9b66d5c3b06b8f9a38b704f1f11aa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "263b74f7ec047cad10454df0534ef846",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4a9d749afe57b6a532d7",
+    "url": "/static/css/main.a9089c74.chunk.css"
+  },
+  {
+    "revision": "8ae49aac9dab507a9ba8",
+    "url": "/static/js/2.18b2a2a8.chunk.js"
+  },
+  {
+    "revision": "4a9d749afe57b6a532d7",
+    "url": "/static/js/main.8b096e95.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
